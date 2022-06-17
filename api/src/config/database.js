b7711c0b18979b2f72e7378931d573c3986d324c
fotini <f.batzoni@gmail.com>
2022-06-17 14:49:49 +0300

    database config

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,12 +1,16 @@
 const Sequelize = require('sequelize');
+const config = require('config');
+
+const dbConfig = config.get('database');
 
 //1st parameter - Db name ("hoxify")
 //2nd parameter - user-name
 //3rd paramater - Db password
 //4th parameter - an object
-const sequelize = new Sequelize('hoxify', 'my-db-user', 'db-p4ss', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  storage: dbConfig.storage,
+  logging: dbConfig.logging,
 });
 
 module.exports = sequelize;
