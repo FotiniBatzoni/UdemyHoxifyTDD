19c2de7b839b561169eb5d71ed99771f4a16e87b
fotini <f.batzoni@gmail.com>
2022-06-21 16:16:22 +0300

    dynamic tests

--- api/src/user/UserRouter.js
+++ api/src/user/UserRouter.js
@@ -24,18 +24,21 @@ const { check, validationResult } = require('express-validator');
 //   next();
 // };
 
-router.post('/api/1.0/users', 
-check('username').notEmpty().withMessage('Username cannot be null'), 
-check('email').notEmpty().withMessage('Email cannot be null'), 
-async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const validationErrors = {};
-    errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
-    return res.status(400).send({ validationErrors: validationErrors });
+router.post(
+  '/api/1.0/users',
+  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('email').notEmpty().withMessage('Email cannot be null'),
+  check('password').notEmpty().withMessage('Password cannot be null'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
+    await UserService.save(req.body);
+    return res.send({ message: 'User Created' });
   }
-  await UserService.save(req.body);
-  return res.send({ message: 'User Created' });
-});
+);
 
 module.exports = router;
