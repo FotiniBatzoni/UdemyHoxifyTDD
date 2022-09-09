c640f1d02b688a9b215570aaddad77be1c0550c3
fotini <f.batzoni@gmail.com>
2022-09-09 13:37:37 +0300

    changing page size

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
+const User = require('./src/user/User');
+
+const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+    // const hash = await bcrypt.hash('P4ssword', 10);
+     for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
+       await User.create({
+         username: `user${i + 1}`,
+         email: `user${i + 1}@mail.com`,
+         inactive: i >= activeUserCount,
+        // password: hash,
+       });
+     }
+   };
 
 //force the database to synchronise with the changes in models
-sequelize.sync({ force: true });
+sequelize.sync({ force: true }).then( async () => {
+    await addUsers(25)
+});
 
 app.listen(3200, () => console.log(`app is running `));
