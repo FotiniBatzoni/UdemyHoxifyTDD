5dc1c1502aa9b1e18989a4e211d6754a80e743d9
fotini <f.batzoni@gmail.com>
2022-09-12 13:42:22 +0300

    middleware basic authentication

--- api/src/user/UserRouter.js
+++ api/src/user/UserRouter.js
@@ -6,7 +6,7 @@ const ValidationException = require('../error/ValidationException');
 const pagination = require('../middleware/pagination');
 const UserNotFoundException = require('./UserNotFoundException');
 const ForbiddenException = require('../error/ForbiddenException');
-const bcrypt = require('bcrypt');
+const basicAuthentication = require('../middleware/basicAuthentication');
 
 // const validateUsername = (req, res, next) => {
 //   const user = req.body;
@@ -105,34 +105,18 @@ router.get('/api/1.0/users/:id', async (req, res, next) => {
 });
 
 
-router.put('/api/1.0/users/:id',  async (req,res,next) => {
-  const authorization = req.headers.authorization;
-  if(authorization){
-    const encoded = authorization.substring(6);
-    const decoded = Buffer.from(encoded, 'base64').toString('ascii');
-    const [email, password] = decoded.split(':');
-    const user = await UserService.findByEmail(email);
-    if(!user){
-      return next(new ForbiddenException('unauthorised_user_update'));
-    }
+router.put('/api/1.0/users/:id', basicAuthentication, async (req,res,next) => {
+  const authedicatedUser = req.authedicatedUser;
 
-    if(user.id != req.params.id){
-      return next(new ForbiddenException('unauthorised_user_update'));
-    } 
-
-    if(user.inactive){
-      return next(new ForbiddenException('unauthorised_user_update'));
-    }
-    const match = await bcrypt.compare(password, user.password);
-    if(!match){
-      return next(new ForbiddenException('unauthorised_user_update'));
-    }
-
-    await UserService.updateUser(req.params.id, req.body);
-
-    return res.send();
+  if(!authedicatedUser || authedicatedUser.id != req.params.id ){
+    
+    return next(new ForbiddenException('unauthorised_user_update'));
   }
-  return next(new ForbiddenException('unauthorised_user_update'));
+
+  await UserService.updateUser(req.params.id, req.body);
+
+  return res.send();
+
 });
 
 
