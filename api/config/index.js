582376bac0473c8fb1b005985bcf294035747605
fotini <f.batzoni@gmail.com>
2022-09-20 16:29:47 +0300

    refactoring configuration

---
+++ api/config/index.js
@@ -0,0 +1,10 @@
+const development = require('./development');
+const staging = require('./staging');
+const test = require('./test');
+
+module.exports = {
+    development,
+    staging,
+    test
+
+}
