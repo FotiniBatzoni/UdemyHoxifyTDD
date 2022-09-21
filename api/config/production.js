809c545a8e52f2efbdd777b23871cfa8b9b6aae1
fotini <f.batzoni@gmail.com>
2022-09-21 13:26:46 +0300

    Organizeing NPM scripts

---
+++ api/config/production.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "database":{
+        "database":"hoaxify",
+        "username":"my-db-user",
+        "password":"db-p4ss",
+        "dialect":"sqlite",
+        "storage":"./prod-db.sqlite",
+        "logging":false
+    },
+    "mail":{ 
+            "host": "smpt.gmail.com",
+            "port": 587,
+            "secure": false,
+              "auth": {
+                "user": "f.batzoni@gmail.com",
+                "pass": "tddcdklrokotihqg"
+              }
+    },
+    uploadDir : 'uploads-production',
+    profileDir : 'profile'
+}
