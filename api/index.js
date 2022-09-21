29f1129119b2f972dda303486d7ce64498198aa4
fotini <f.batzoni@gmail.com>
2022-09-21 14:37:07 +0300

    Adding PORT

--- api/index.js
+++ api/index.js
@@ -33,4 +33,4 @@ sequelize.sync();
 
 TokenService.scheduleCleanup();
 
-app.listen(3200, () => logger.info(`app is running!  version: ` + process.env.npm_package_version));
+app.listen(process.env.PORT || 3200, () => logger.info(`app is running!  version: ` + process.env.npm_package_version));
