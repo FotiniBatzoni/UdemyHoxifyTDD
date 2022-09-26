5a0499699067c5e0fe8369425e49ad7d528c6e43
fotini <f.batzoni@gmail.com>
2022-09-26 12:37:20 +0300

    Serving attachment folder

--- api/src/app.js
+++ api/src/app.js
@@ -13,8 +13,10 @@ const tokenAuthentication = require('./middleware/tokenAuthentication');
 const FileService = require('./file/FileService');
 const config = require('config');
 const path = require('path');
-const { uploadDir, profileDir } = config;
+
+const { uploadDir, profileDir,attachmentDir } = config;
 const profileFolder = path.join('.', uploadDir, profileDir);
+const attachmentFolder = path.join('.', uploadDir, attachmentDir);
 
 const ONE_YEAR_IN_MILLIS = 365 * 24 * 60 *60 * 1000;
 
@@ -46,6 +48,8 @@ app.use(middleware.handle(i18next));
 app.use(express.json({ limit: '3mb'}));
 
 app.use('/images', express.static(profileFolder, { maxAge: ONE_YEAR_IN_MILLIS  }));
+
+app.use('/attachments', express.static(attachmentFolder, { maxAge: ONE_YEAR_IN_MILLIS  }));
 
 app.use(tokenAuthentication);
 
