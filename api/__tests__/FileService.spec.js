9d05c8dd1bdd09ae16c2af928130442726857116
fotini <f.batzoni@gmail.com>
2022-09-27 15:17:45 +0300

    keeps the files younger than 24 hours and thei database entry even not associated with hoax

--- api/__tests__/FileService.spec.js
+++ api/__tests__/FileService.spec.js
@@ -29,14 +29,25 @@ describe('createFolders', () =>{
 });
 
 describe('Schedule unused file clean up' , (done) =>{
+
+    const filename = 'test-file'+ Date.now();
+    const testFile = path.join('.','__tests__','resources','test-png.png'); 
+    const targetPath = path.join(attachmentFolder, filename);
+
+    beforeEach( async () =>{
+        await FileAttachment.destroy({ truncate: true});
+        if(fs.existsSync(targetPath)){
+            fs.unlinkSync(targetPath);
+        }
+    });
+
     it('removes the 24 hours old file with attachment entry if not used in hoax', async () =>{
         jest.useFakeTimers()
-       const testFile = path.join('.','__tests__','resources','test-png.png'); 
-       const targetPath = path.join(attachmentFolder, 'test-file1');
+
        fs.copyFileSync(testFile, targetPath);
        const uploadDate = new Date(Date.now() - 24 * 60 * 60 * 1000 -1 );
        const attachment = await FileAttachment.create({
-            filename :'test-file1',
+            filename : filename,
             uploadDate: uploadDate
        })
        await FileService.removeUnusedAttachments();
@@ -46,6 +57,27 @@ describe('Schedule unused file clean up' , (done) =>{
             const attachmentAfterRemove = await FileAttachment.findOne({ where: { id: attachment.id}});
             expect(attachmentAfterRemove).toBeNull();
             expect(fs.existsSync(targetPath)).toBe(false);
+            done();
+       },1000)
+
+    });
+
+    it('keeps the files younger than 24 hours and thei database entry even not associated with hoax', async () =>{
+        jest.useFakeTimers()
+
+       fs.copyFileSync(testFile, targetPath);
+       const uploadDate = new Date(Date.now() - 23 * 60 * 60 * 1000 -1 );
+       const attachment = await FileAttachment.create({
+            filename : filename,
+            uploadDate: uploadDate
+       })
+       await FileService.removeUnusedAttachments();
+       jest.advanceTimersByTime(24  * 60 * 60 * 1000 + 5000);
+       //jest.useRealTimers();
+       setTimeout( async () =>{
+            const attachmentAfterRemove = await FileAttachment.findOne({ where: { id: attachment.id}});
+            expect(attachmentAfterRemove).not.toBeNull();
+            expect(fs.existsSync(targetPath)).toBe(true);
             done();
        },1000)
 
