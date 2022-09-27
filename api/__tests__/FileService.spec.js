cb6736858a96231c65c43030e93467151b7f1e87
fotini <f.batzoni@gmail.com>
2022-09-27 15:29:03 +0300

    Checking Unused Attachments

--- api/__tests__/FileService.spec.js
+++ api/__tests__/FileService.spec.js
@@ -1,6 +1,7 @@
 const FileService = require('../src/file/FileService');
 const FileAttachment = require('../src/file/FileAttachment');
 const Hoax = require('../src/hoax/Hoax');
+const User = require('../src/user/User');
 const fs = require('fs');
 const path = require('path');
 const config = require('config');
@@ -36,10 +37,26 @@ describe('Schedule unused file clean up' , (done) =>{
 
     beforeEach( async () =>{
         await FileAttachment.destroy({ truncate: true});
+        await User.destroy({ truncate: { cascade: true }})
         if(fs.existsSync(targetPath)){
             fs.unlinkSync(targetPath);
         }
     });
+
+    const addHoax = async (  ) => {
+      
+          const user = await User.create({
+            username: `user1`,
+            email: `user1@mail.com`
+          });
+          const hoax = await Hoax.create({
+            content: `hoax content 1`,
+            timestamp: Date.now(),
+            userId: user.id
+          });
+ 
+        return hoax.id;
+      };
 
     it('removes the 24 hours old file with attachment entry if not used in hoax', async () =>{
         jest.useFakeTimers()
@@ -81,5 +98,28 @@ describe('Schedule unused file clean up' , (done) =>{
             done();
        },1000)
 
-    })
+    });
+
+    it('keeps the files older than 24 hours and thei database entry if associated with hoax', async () =>{
+        jest.useFakeTimers()
+
+       fs.copyFileSync(testFile, targetPath);
+       const id = await addHoax();
+       const uploadDate = new Date(Date.now() - 23 * 60 * 60 * 1000 -1 );
+       const attachment = await FileAttachment.create({
+            filename : filename,
+            uploadDate: uploadDate,
+            hoaxId: id
+       })
+       await FileService.removeUnusedAttachments();
+       jest.advanceTimersByTime(24  * 60 * 60 * 1000 + 5000);
+       //jest.useRealTimers();
+       setTimeout( async () =>{
+            const attachmentAfterRemove = await FileAttachment.findOne({ where: { id: attachment.id}});
+            expect(attachmentAfterRemove).not.toBeNull();
+            expect(fs.existsSync(targetPath)).toBe(true);
+            done();
+       },1000)
+
+    });
 })
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 const TokenService = require('./src/auth/TokenService');
+const FileService = require('./src/file/FileService');
 const logger = require('./src/shared/logger');
 
 
@@ -32,5 +33,6 @@ sequelize.sync();
 
 
 TokenService.scheduleCleanup();
+FileService.removeUnusedAttachments();
 
 app.listen(process.env.PORT || 3200, () => logger.info(`app is running!  version: ` + process.env.npm_package_version));
