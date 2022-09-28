74bf7cce8a59303a5bac3a5107cb2b0fbd00fe94
fotini <f.batzoni@gmail.com>
2022-09-28 14:04:15 +0300

    Hoax Delete Error Cases

--- api/__tests__/HoaxDelete.spec.js
+++ api/__tests__/HoaxDelete.spec.js
@@ -27,6 +27,14 @@ const addUser = async (user = {...activeUser}) =>{
     const hash = await bcrypt.hash(user.password,10);
     user.password = hash;
     return await User.create(user);
+};
+
+const addHoax = async (userId )=>{
+  return  await Hoax.create({
+        content: 'Hoax for user',
+        timestamp: Date.now(),
+        userId: userId
+    })
 }
 
 const auth = async (options = {}) => {
@@ -74,5 +82,14 @@ describe('Delete Hoax', () =>{
         expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
         expect(response.body.message).toBe(message);
       });
+
+      it('returns 403 when user tries to delete another user s hoax', async() =>{
+        const user = await addUser();
+        const hoax = await addHoax(user.id);
+        const user2 = await addUser({ ... activeUser, username:'user2',email:'user2@mail.com'});
+        const token = await auth({ auth: {email: user2.email, password: 'P4ssword'}});
+        const response = await deleteHoax( hoax.id, {token});
+        expect(response.status).toBe(403);
+      })
 })
 
